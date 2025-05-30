e559d2d96ef7a0f37fed0b406ab62c945653e541
Jauhari007 <achmadpradana39@gmail.com>
2025-05-31 04:01:03 +0700

    menambahkan fitur project cv & detail us cv pada page mandor

--- views/pages/mandor/dashboard.php
+++ views/pages/mandor/dashboard.php
@@ -1,7 +1,6 @@
-<div class="header-bg">
+<link rel="stylesheet" href="/public/mandor.css">
     <h1>Dashboard Mandor</h1>
     <h2>Selamat datang, Mandor!</h2>
 </div>
 
 <?php include __DIR__ . "/../../components/alert.php" ?>
-<link rel="stylesheet" href="/css/mandor.css">
